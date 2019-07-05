93748deaf4db144306754eab4214ba3af5d83345
webturing <zj@webturing.com>
2019-07-05 13:42:51 +0800

    An Simple Application for Input/Output:

--- src/lec01a/FirstApplication.java
+++ src/lec01a/FirstApplication.java
@@ -1,7 +1,13 @@
 package lec01a;
 
+import java.util.Scanner;
+
 public class FirstApplication {
     public static void main(String[] args) {
-
+        Scanner cin = new Scanner(System.in);
+        String name = cin.next();
+        int year = cin.nextInt();
+        System.out.println("Hello " + name + "!");
+        System.out.println(String.format("You are %d year(s) old.", 2019 - year));
     }
 }
